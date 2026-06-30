4b4cd97765e2a8d18a8ace872553f54d1e7a087d
Jeff Hardesty <jahardesty7@gmail.com>
2026-06-30 15:51:23 -0400

    Initial Commit

---
+++ Retrocade/RetrocadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetrocadeApp.swift
+//  Retrocade
+//
+//  Created by Jeff on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct RetrocadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
